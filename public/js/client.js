fc289658d3790f356cea1c1a17287c2b13082b3c
Lucas Carr <lukegabrielcarr@gmail.com>
2023-05-14 20:35:18 +0200

    fixed issue with unresponsive screen

--- public/js/client.js
+++ public/js/client.js
@@ -1,8 +1,24 @@
 const searchForm = document.querySelector('#search-form');
 const searchInput = document.querySelector('#search-input');
 
+const animationContainer = d3.select("#animation-container");
+
+const settingsButton = document.querySelector('.settings-button');
+const settingsPanel = document.querySelector('.settings-panel');
+
+function clearTextbox(textboxId) {
+  document.getElementById(textboxId).value = "";
+}
+
+
+settingsButton.addEventListener('click', () => {
+  settingsPanel.style.display = settingsPanel.style.display === 'none' ? 'block' : 'none';
+});
+
+
 searchForm.addEventListener('submit', (event) => {
   event.preventDefault();
+  event.stopPropagation();
   const queryText = searchInput.value;
   sendRequest(queryText);
 });
@@ -15,13 +31,14 @@ function sendRequest(query) {
   var results_array = [];
 
   fetch(url)
-    .then((response) => {
+    .then(response => {
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
       return response.json();
     })
-    .then((responseJSON) => {
+    .then(responseJSON => {
+      // Do something with the responseJSON
       let resultCounter = 1;
       data = convertJson(JSON.stringify(responseJSON));
       console.log(data)
@@ -38,11 +55,46 @@ function sendRequest(query) {
       results_array.forEach(element => {
         console.log(JSON.stringify(element));
       });
+      console.log(responseJSON);
     })
-    .catch((error) => {
+    .catch(error => {
       console.error(`Error sending request: ${error}`);
     });
 }
+
+// function sendRequest(query) {
+//   const url = `http://localhost:3000/search?q=${query}`;
+//   var results_array = [];
+
+//   fetch(url)
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error(`HTTP error! status: ${response.status}`);
+//       }
+//       return response.json();
+//     })
+//     .then((responseJSON) => {
+//       let resultCounter = 1;
+//       data = convertJson(JSON.stringify(responseJSON));
+//       console.log(data)
+//       bubbles(data);
+//       relevance = 50;
+//       responseJSON.forEach((result) => {
+//         let resultText = `result ${resultCounter++}:\n`;
+//         for (let key in result) {
+//           resultText += `${key}: ${JSON.stringify(result[key])}`;
+//         }
+//         const resultNode = document.createElement('pre');
+//         resultNode.textContent = resultText;
+//       });
+//       results_array.forEach(element => {
+//         console.log(JSON.stringify(element));
+//       });
+//     })
+//     .catch((error) => {
+//       console.error(`Error sending request: ${error}`);
+//     });
+// }
 
 function convertJson(jsonStr) {
   let jsonData = JSON.parse(jsonStr);
@@ -75,11 +127,12 @@ function convertJson(jsonStr) {
 }
 
 var nodeBox = document.getElementById("nodes-box").getBoundingClientRect();
-var middle_h = 1200;
+var middle_h = nodeBox.height;
 var middle_w = nodeBox.width;
 
 function bubbles(data) {
-  d3.select("body").select("svg").remove();
+
+  d3.select("#animation-container").select("svg");
   var width = middle_w;
   var height = middle_h;
   var nodes = [];
@@ -87,25 +140,26 @@ function bubbles(data) {
   var foci = [
     { x: width / 2, y: height / 2 }
   ];
-
-  var svg = d3.select("body")
+  
+  var svg = d3.select("#animation-container")
     .append("svg")
     .attr("width", width)
     .attr("height", height)
     .attr("class", "mySvg") // add class to SVG
     .style("overflow", "hidden");
+  
 
   var force = d3.layout.force()
     .nodes(nodes)
     .links([])
     .charge(-1000) // Increase the magnitude of negative charge for more repulsion
-    .gravity(0.005) // Reduce the gravity to let the nodes spread out more
+    .gravity(0.001) // Reduce the gravity to let the nodes spread out more
     .chargeDistance(width) // Set the chargeDistance to half the width of the screen
     .size([width, height])
     .on("tick", tick);
   var counter = 0;
   var node = svg.selectAll("g");
-  
+
   function tick(e) {
     var k = .1 * e.alpha;
     // Push nodes toward their designated focus.
@@ -114,22 +168,22 @@ function bubbles(data) {
       o.x += (foci[0].x - o.x) * k;
       // Ensure the nodes stay within the bounds of the SVG
       o.x = Math.max(o.x, 200);
-      o.y = Math.max(o.y, 250);
-      o.x = Math.min(o.x, 900);
-      o.y = Math.min(o.y, 825);
+      o.y = Math.max(o.y, 0);
+      o.x = Math.min(o.x, 1000);
+      o.y = Math.min(o.y, 650);
     });
     node.attr("transform", function (d) {
       // Ensure the nodes stay within the bounds of the SVG
       d.x = Math.max(d.x, 200);
-      d.y = Math.max(d.y, 250);
+      d.y = Math.max(d.y, 100);
       d.x = Math.min(d.x, 900);
       d.y = Math.min(d.y, 825);
       return "translate(" + d.x + "," + d.y + ")";
     });
   }
-  
 
-  var timer = setInterval(function() {
+
+  var timer = setInterval(function () {
     if (nodes.length > data.length - 1) {
       clearInterval(timer);
       return;
@@ -144,16 +198,16 @@ function bubbles(data) {
       date: item.date
     });
     force.start();
-  
+
     node = node.data(nodes);
     var prev_title = "";
     var n = node.enter().append("g")
       .attr("class", "node")
-      .attr("transform", function(d) {
+      .attr("transform", function (d) {
         return "translate(" + d.x + "," + d.y + ")";
       })
       .style('cursor', 'pointer')
-      .on('mousedown', function(d) {
+      .on('mousedown', function (d) {
         var sel = d3.select(this);
         sel.moveToFront();
         var infoElement = document.getElementById("bubble-info");
@@ -162,13 +216,13 @@ function bubbles(data) {
           prev_title = "";
         } else {
           infoElement.innerHTML = "Title: " + d.title + "<br> <br>" +
-            "Subjects: " + d.subject  + "<br> <br>" + 
+            "Subjects: " + d.subject + "<br> <br>" +
             "Date: " + d.date;
           prev_title = d.title;
         }
       })
       .call(force.drag);
-  
+
     // define the color scale with linear interpolation
     var colorScale = d3.scale.linear()
       .domain([50, 1])
@@ -176,48 +230,45 @@ function bubbles(data) {
 
     // create the circles
     n.append("circle")
-      .attr("r", function(d) {
+      .attr("r", function (d) {
         return d.relevance * 2.5;
       })
-      .style("fill", function(d) {
+      .style("fill", function (d) {
         return colorScale(d.relevance);
       });
 
-
-
-
-  
     n.append("foreignObject")
-      .attr("x", function(d) {
+      .attr("x", function (d) {
         return -d.relevance * 2;
       })
-      .attr("y", function(d) {
+      .attr("y", function (d) {
         return -d.relevance * 2;
       })
-      .attr("width", function(d) {
+      .attr("width", function (d) {
         return d.relevance * 4;
       })
-      .attr("height", function(d) {
+      .attr("height", function (d) {
         return d.relevance * 4;
       })
-      .html(function(d) {
+      .html(function (d) {
         return "<div style='width:" + (d.relevance * 4) + "px; height:" + (d.relevance * 4) + "px; text-align:center; display: flex; color: #fbf0e0; justify-content: center; align-items: center;'>" +
           "<span>" + d.title + "</span>" +
           "</div>";
       });
     counter++;
-  }, 100);
-  
-  d3.selection.prototype.moveToFront = function() {
-    return this.each(function() {
+  }, 50);
+
+  d3.selection.prototype.moveToFront = function () {
+    return this.each(function () {
       this.parentNode.appendChild(this);
     });
   };
-  
+
   function resize() {
     width = window.innerWidth;
     force.size([width, height]);
     force.start();
   }
   d3.select(window).on('resize', resize);
+  
 };  
